426efd44366b0a8e5c4af7076e17055234fcc3d8
matt <knbdmb@yahoo.com>
2022-11-25 22:52:15 -0800

    playing with pivot tables and plotting
    replcated libreoffice pivot tables and charts
    
    A Guide to Pandas Pivot Table
    https://builtin.com/data-science/pandas-pivot-tables

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def import_data():
     text_file.write(html)
     text_file.close()
 
-    pt_plot = pt_yyyy_type.plot.barh(figsize=(10,16))
+    pt_plot = pt_yyyy_type.plot.barh(figsize=(10,16), stacked=True)
     fig = pt_plot.get_figure()
     fig.savefig("pt_yyyy_type.png")
 
