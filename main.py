3ca58e1bbc95962a0836ae5b4951aee59fcb7980
matt <knbdmb@yahoo.com>
2023-01-04 20:56:39 -0800

    monthly charts task plotting now working on available hours chart

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ def create_monthly_display():
                 print(df_date)
                 #date_given = datetime.datetime(year=2019, month=7, day=30).date()
                 print("\nWeek number of month: ", week_number_of_month(df_date), "\n")
-                time.sleep(2)
+                time.sleep(1)
                 # fill in balance focus details
                 # save drawing obj to file
                 file_output = current_month + "_rpt.svg"
@@ -223,7 +223,7 @@ def create_monthly_display():
             focus_hours_logged[0] = 0
             for i in range(1, month_number_of_days + 1):
                 print(i)
-                if df_date.day = 6:
+                if df_date.day == 6:
                     available_hours[i] = 8
                     available_cumulative_hours[i] = available_hours[i] + available_cumulative_hours[i-1]
                     focus_hours[i] = 3
@@ -237,7 +237,11 @@ def create_monthly_display():
 
                 available_hours_logged[i] = 0
                 focus_hours_logged[i] = 0
-            #time.sleep(2)
+            print(available_hours)
+            print(available_cumulative_hours)
+            print(focus_hours)
+            print(focus_cumulative_hours)
+            time.sleep(2)
             # clean previous month info and chart
             current_month_x_offset = month_chart_offset_x
             current_month_y_offset = month_chart_offset_y
